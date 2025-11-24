6e39087715ad89084cd6b366c4b15d144ff2a0b3
angelluisss <angelluiscastiblanco@gmail.com>
2025-11-24 14:24:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,28 +8,45 @@ function preload() {
 }
 
 function setup() {
-  
   createCanvas(windowWidth, windowHeight);
   textSize(16);
   textAlign(LEFT, TOP);
 
-  if (DeviceOrientationEvent.requestPermission) {
+  // BOTÓN PARA ACTIVAR AUDIO (OBLIGATORIO EN CELULAR)
+  let btn = createButton("🔊 Activar audio");
+  btn.position(20, 120);
+  btn.mousePressed(() => {
+    userStartAudio(); 
+    console.log("Audio habilitado");
+  });
+
+  // PERMISO PARA USAR GIROSCOPIO EN iPHONE
+  if (typeof DeviceOrientationEvent !== "undefined" &&
+      typeof DeviceOrientationEvent.requestPermission === "function") {
+
     DeviceOrientationEvent.requestPermission().then(response => {
-      if (response === 'granted') {
-        window.addEventListener('deviceorientation', readOrientation);
+      if (response === "granted") {
+        window.addEventListener("deviceorientation", readOrientation);
+      } else {
+        alert("No se otorgó permiso para acceder al giroscopio.");
       }
     });
+
   } else {
-    window.addEventListener('deviceorientation', readOrientation);
+    // ANDROID / PC
+    window.addEventListener("deviceorientation", readOrientation);
   }
 }
 
 function draw() {
   background(200);
+
+  // Mostrar valores del giroscopio
   text(`Alpha: ${alpha}`, 20, 20);
   text(`Beta: ${beta}`, 20, 50);
   text(`Gamma: ${gamma}`, 20, 80);
 
+  // Activar sonido si el celular se gira
   if (alpha > 90) {
     if (!sound.isPlaying()) {
       sound.play();
@@ -37,29 +54,6 @@ function draw() {
   } else {
     if (sound.isPlaying()) {
       sound.stop();
-      function setup() {
-  createCanvas(windowWidth, windowHeight);
-  textSize(16);
-  textAlign(LEFT, TOP);
-
-  let btn = createButton("Activar audio");
-  btn.position(20, 120);
-  btn.mousePressed(() => {
-    userStartAudio();
-    console.log("Audio activado");
-  });
-
-  if (DeviceOrientationEvent.requestPermission) {
-    DeviceOrientationEvent.requestPermission().then(response => {
-      if (response === 'granted') {
-        window.addEventListener('deviceorientation', readOrientation);
-      }
-    });
-  } else {
-    window.addEventListener('deviceorientation', readOrientation);
-  }
-}
-
     }
   }
 }
@@ -68,18 +62,4 @@ function readOrientation(e) {
   alpha = e.alpha;
   beta = e.beta;
   gamma = e.gamma;
-
-  function setup() {
-  createCanvas(windowWidth, windowHeight);
-  let btn = createButton("Activar audio");
-  btn.position(20, 120);
-  btn.mousePressed(() => {
-    userStartAudio();
-    sound.play();
-    sound.stop();
-  });
-
-  // ... resto
-}
-
 }
